fd1fb030303ce5b7e53d07214b6ee3579cbea1db
HemanthKumar32 <payalforjain@gmail.com>
2017-07-20 19:20:48 -0400

    merge Sorted Arrays

--- JAVA_DS_Maven/src/leetcode/easy/arrays/MergeTwoSortedArrays.java
+++ JAVA_DS_Maven/src/leetcode/easy/arrays/MergeTwoSortedArrays.java
@@ -9,5 +9,32 @@ public class MergeTwoSortedArrays {
 		while (j > -1)
 			A[k--] = B[j--];
 	}
+	
+	
+	// Betterr solution
+	
+	public static  int[] mergeBetter(int A[], int m, int B[], int n) {
+		int i = m - 1, j = n - 1, k = m + n - 1;
+		int[] X = new int[m+n];
+		while (i > -1 && j > -1)
+			X[k--] = (A[i] > B[j]) ? A[i--] : B[j--];
+		while (j > -1)
+			X[k--] = B[j--];
+		// Mandatory Condition
+		while (i > -1)
+			X[k--] = A[i--];
+		
+		return X;
+	}
+
+	
+	
+	public static void main(String[] args)
+	{
+		int[] a = {1,4,5};
+		int[] b = {3,4,7,8};
+		System.out.println(mergeBetter(a,3,b,4));
+		
+	}
 
 }
